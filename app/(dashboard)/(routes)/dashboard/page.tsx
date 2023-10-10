5e4b0eb5113b8193b2b0e8461beb0f4008616caa
Christian Gonzales <chrisgonzales.online@gmail.com>
2023-10-10 20:54:15 +0800

    Complete sidebar with router

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard Page (protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
@@ -5,11 +8,11 @@ interface DashboardLayoutProps {
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
     <div className="h-full relative">
-      <aside className="hidden h-full md:flex md:w-72  md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        <div>Hello sidebar</div>
-      </aside>
+      <div className="hidden h-full md:flex md:w-72  md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+        <Sidebar />
+      </div>
       <main className="md:pl-72">
-        Hello Content
+        <Navbar />
         {children}
       </main>
     </div>
